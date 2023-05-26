94e4bfd404ab4c65f492d01a8c363f6d226876a0
apexSnehith <snehith0713@gmail.com>
2023-05-26 12:01:27 +0530

    sample sh

---
+++ print.sh
@@ -0,0 +1,7 @@
+echo Hello world, this is snehith
+#using colors
+#syntax : echo -e "\e[COLmMESSAGE\e[0m"
+
+echo -e "\e[31mthis is red\e[0m"
+echo -e "\e[32mthis is green\e[0m"
+echo -e "\e[33mthis is red\e[0m"
--- sample.sh
+++ sample.sh
@@ -2,5 +2,5 @@ pwd
 ls -ls
 touch sample.txt
 echo "this is a sample txt" >> sample.txt
-
+#
 yum list installed | grep sudo
