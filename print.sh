45f528ba5f86c5ab056a10dfc5a18d2c70b0fad2
apexSnehith <snehith0713@gmail.com>
2023-05-26 12:03:48 +0530

    sample sh

--- print.sh
+++ print.sh
@@ -4,4 +4,7 @@ echo Hello world, this is snehith
 
 echo -e "\e[31mthis is red\e[0m"
 echo -e "\e[32mthis is green\e[0m"
-echo -e "\e[33mthis is red\e[0m"
+echo -e "\e[33mthis is yellow\e[0m"
+echo -e "\e[34mthis is blue\e[0m"
+echo -e "\e[35mthis is magenta\e[0m"
+echo -e "\e[36mthis is cyan\e[0m"
