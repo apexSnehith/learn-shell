41b4e92913430893fa263881d57ad9116f5b3d9d
apexSnehith <snehith0713@gmail.com>
2023-09-27 23:12:53 +0530

    learn-shell conditions

--- cond.sh
+++ cond.sh
@@ -1,8 +1,8 @@
 animal=$1
 
-if [ $animal == 'lion']; then
+if [ $animal == 'lion' ]; then
   echo animal name is 1$
 fi
-if [ $animal == 'tiger']; then
+if [ $animal == 'tiger' ]; then
   echo animal name is 2$
 fi
